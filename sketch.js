adb04972555e79ce7aa3f6a9d85fbb3122d19b4e
Pablo Cieslik <pablocieslik@gmail.com>
2023-07-15 04:47:34 -0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    // Este es un buen lugar para inicializar el tamaño del lienzo, cargar imágenes, etc.
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    // Este es un buen lugar para dibujar formas, actualizar variables, manejar interacciones, etc.
+    background(220);
+  }
+  
