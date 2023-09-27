c224098b767225ee0af24c9960c9ff89f8000839
Pablo Cieslik <75939161+al-hum@users.noreply.github.com>
2023-09-27 19:41:47 -0300

    Update script.js
    Signed-off-by: Pablo Cieslik <75939161+al-hum@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const startButton = document.getElementById("startButton");
   startButton.addEventListener("click", function() {
     // Navigate to the next page
-    window.location.href = "../2page2/2page2.html"; // Assuming the next page is named "2page2.html"
+    window.location.href = "2page2.html"; // Assuming the next page is named "2page2.html"
   });
 });
