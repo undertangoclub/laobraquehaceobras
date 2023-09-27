1a810bd2f3bcc01117bdcbc11a99fe37dd91acdb
Pablo Cieslik <75939161+al-hum@users.noreply.github.com>
2023-09-27 19:45:28 -0300

    Update sketch.js
    Signed-off-by: Pablo Cieslik <75939161+al-hum@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const startButton = document.getElementById("startButton");
   startButton.addEventListener("click", function() {
     // Navigate to the next page
-    window.location.href = "../2page2/2page2.html"; // Assuming the next page is named "2page2.html"
+    window.location.href = "2page2/2page2.html"; // Assuming the next page is named "2page2.html"
   });
 });
 
