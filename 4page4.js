0e67153af3ccba077c94acbdc39af81a1819cf39
Pablo Cieslik <75939161+al-hum@users.noreply.github.com>
2023-09-27 20:34:04 -0300

    Update 4page4.js
    Signed-off-by: Pablo Cieslik <75939161+al-hum@users.noreply.github.com>

--- 4page4.js
+++ 4page4.js
@@ -114,7 +114,6 @@ window.addEventListener('load', function() {
   const combineButton = document.getElementById("combineButton");
 
 document.addEventListener("DOMContentLoaded", function() {
-  const combineButton = document.getElementById("combineButton");
   combineButton.addEventListener("click", function() {
     // Navigate to 5page5
     console.log("Redirecting to:", "5page5.html");
