c6e1df28c31755f0efab346b9c7fe23528e28bcd
Pablo Cieslik <75939161+al-hum@users.noreply.github.com>
2023-09-27 19:40:42 -0300

    Create 2page2.js
    Signed-off-by: Pablo Cieslik <75939161+al-hum@users.noreply.github.com>

---
+++ 2page2.js
@@ -0,0 +1,20 @@
+/* The code you provided is written in JavaScript and it is adding an event listener to the
+"DOMContentLoaded" event of the document object. This event is fired when the initial HTML document
+has been completely loaded and parsed. */
+document.addEventListener("DOMContentLoaded", function() {
+    const nextButton = document.getElementById("nextButton");
+    nextButton.addEventListener("click", function() {
+      // Get user input
+      const userPrompt = document.getElementById("userPrompt").value;
+      
+      // You can store this value in local storage or pass it to the next page
+      localStorage.setItem("userPrompt", userPrompt);
+  
+      // Navigate to the next page
+      window.location.href = "../3page3/3page3.html";// Assuming the next page is named "3page3.html"
+    });
+  });
+  
+  // Save user input to localStorage
+const userPrompt = document.getElementById('userPrompt').value;
+localStorage.setItem('userPrompt', userPrompt);
